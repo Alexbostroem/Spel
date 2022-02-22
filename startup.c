00bb04272869997cb8479acea4b32c6d91f54b92
Johanna källström <johanna_kallstrom@hotmail.com>
2022-02-22 13:40:01 +0100

    startup

--- startup.c
+++ startup.c
@@ -22,9 +22,31 @@ __asm__ volatile(".L1: B .L1\n");				/* never return */
 
 #include "define_global.h"
 #include "ascii_display.h"
+#include "grafik.h"
+#include "graphic_driver.h"
+
+void app_init(void) {
+	*GPIO_D_MODER &= 0x00000000;	// Nollställer bit 31-16 resten kvarstår
+	*GPIO_D_MODER |= 0x55005555;	// 0101 0101 0101 0101 (bit 31-16) resten kvarstår, bit 31-16 utsignaler med varje pin representerade med 2 bitar
+	*GPIO_D_OTYPER &= 0x0000;	//	Nollställ bit 15-12 i OTYPER (push-pull), bit 11-8 kvarstår. utpinnar som push-pull
+	*((unsigned long *) GPIO_D_BASE + 0x0E) &= 0x00000000;
+	*((unsigned long *) GPIO_D_BASE + 0x0E) |= 0x00AA0000;	//	Sätt pull-down (1010) på inpinnar i register PUPDR(2bit/pin)
+	
+}
 
 void main(void)
 {
+	app_init();
+	POBJECT m =&mouse;
+	POBJECT k =&cat;	
 	
+	graphic_initialize();
+	graphic_clear_screen();
+	while(1)
+			{
+				m->move(m);
+				k->move(k);
+				delay_micro(200);
+}
 }
 

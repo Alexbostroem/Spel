9c39c59d39d162970b635af8820cdf0b11d642df
Niklas Serner <niklas.serner1999@gmail.com>
2022-02-25 13:05:02 +0100

    fungerande spel, utan avbrottshantering

--- init.h
+++ init.h
@@ -2,4 +2,6 @@ void keyboard_init(void);
 
 void display_init(void);
 
+void init_app_ascii(void);
+
 void app_init(void);
--- startup.c
+++ startup.c
@@ -28,26 +28,47 @@ __asm__ volatile(".L1: B .L1\n");				/* never return */
 #include "keypad_driver.h"
 #include "pointordeath_function.h"
 
+void start_ascii(void) {
+	char *startword;
+	char ascii_start[] = "Points: "; // ska alltid skrivas vid start av spelet (endast 1 gång)
+	ascii_gotoxy(1,1);		// gå till rad 1, col 1
+    startword = ascii_start;
+	 while (*startword) {
+    	ascii_write_char(*startword++);
+    }
+	 
+}
 
 void main(void)
 {
 	app_init();
+	start_ascii(); // Skriver ut "Points: "
+	
+	char *out_ascii;	// Pekare till den array som ska skrivas till asciidisplay
+	char ascii_points[] = {0x30,'\0'};    // Aktuell poäng, uppdateras under spelets gång
+	char ascii_GO[] = "GAME OVER! ";
+	ascii_gotoxy(9,1);		// gå till col 9 rad 1 på asciidisplayen
+	out_ascii = ascii_points;
+	
+	 while (*out_ascii) {
+    	ascii_write_char(*out_ascii++);
+    }
+	// Ovan går att lägga i start_ascii. Är nollan vid start som skrivs ut efter "Points: "
 	
 	POBJECT m =&mouse;
 	POBJECT d =&damsugare;
 	POBJECT k =&cat;	
 	POBJECT b =&bird;
-
-	
-
+	int write_counter_m = 0;
+	int write_counter_b = 0;
 	char c;
-	int points;
+
 
     while(1){
 		
-		if (points> 20){
-			m->set_speed(m,0,-10);
-			b->set_speed(b,0,-10);
+		if (ascii_points[0] >= 0x35){	// ascii_points[0] är nya points!!
+			m->set_speed(m,-10,0);
+			b->set_speed(b,-10,0);
 
 		}
 		
@@ -65,25 +86,50 @@ void main(void)
 	
 		
 		if (approx(k,m) & k->geo_number==duck){
-					if (exact_objects_overlap(k,m)){
+					//if (exact_objects_overlap(k,m)){
 					m->clear_all(m);
 					m->posx = 130;
+					
+					// EN POÄNG FÖR MUSEN
+					ascii_points[0]++;
+					ascii_gotoxy(9,1);
+					out_ascii = ascii_points;
+					while (*out_ascii) {
+						ascii_write_char(*out_ascii++);
 					}
-		}
+				}
+		//}
 		if (approx(k,b) & k->geo_number==jump){
-					if (exact_objects_overlap(k,b)){
+					//if (exact_objects_overlap(k,b)){
+					
 					b->clear_all(b);
 					b->posx = 130;
+					
+					// TVÅ POÄNG FÖR FÅGEL
+					ascii_points[0] += 2;
+					ascii_gotoxy(9,1);
+					out_ascii = ascii_points;
+					while (*out_ascii) {
+						ascii_write_char(*out_ascii++);
 					}
-		}
+				}
+		//}
 		if (approx(k,d)){
-					if (exact_objects_overlap(k,d)){
+					//if (exact_objects_overlap(k,d)){
 					k->clear_all(k);
 					k->posx = 140;
+					
+					// GAME OVER OM DAMMSUGAREN SUGER I SIG KATTEN
+					ascii_gotoxy(1,1);
+					out_ascii = ascii_GO;
+					while (*out_ascii) {
+						ascii_write_char(*out_ascii++);
 					}
+					
+					//}
 		}
 	}
-
+	
 }
 
 
